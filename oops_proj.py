c7d3fadf5584148fa24199c1aab11ba4c9983e9b
Pramod <pramodswain62@gmail.com>
2026-04-16 20:02:04 +0530

    signed up and signed in added

--- oops_proj.py
+++ oops_proj.py
@@ -13,14 +13,38 @@ class chatbook:
                             "4. Press 4 to message a friend" 
                             "5. Press any other key to exit""")
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == '3':
             pass
         elif user_input == '4':
             pass
         else:
             exit()
+
+    def signup(self):
+        email = input("enter your email here -> ")
+        pwd = input("enter your password -> ")
+        self.username = email
+        self.password = pwd
+        print("You have signed up successfully !!")
+        print("\n")
+        self.menu()
+    
+    def signin(self):
+        if self.username == '' and self.password == '':
+            print("Please signup first by pressing 1 in main menu")
+        else:
+            username = input("enter your email/username here -> ")
+            pwd = input("enter your password -> ")
+            if self.username == username and self.password == pwd:
+                print('You have signed in successfully')
+                self.loggedin = True
+            else:
+                print('Please input the correct credentials')
+        print("\n")
+        self.menu()
+
 
 obj = chatbook()
